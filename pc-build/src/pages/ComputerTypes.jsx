e96e1b9536e643c883213f3631184d6edb59a87e
Gsoetan <43526261+Gsoetan@users.noreply.github.com>
2022-06-12 11:11:57 -0400

    Start of comp types page
    Beginning work on computer types webpage

---
+++ pc-build/src/pages/ComputerTypes.jsx
@@ -0,0 +1,16 @@
+import Avalanche from "../assets/images/Avalanche_Hardline_Liquid_Cooled_Gaming_PC.png";
+import HeaderBanner from "../components/content/HeaderBanner";
+
+export default function ComputerTypes() {
+  return (
+    <div>
+      <div>
+        <HeaderBanner
+          background={Avalanche}
+          title="Build Ideas"
+          description="Take a look at our builds"
+        />
+      </div>
+    </div>
+  );
+}
